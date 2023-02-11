a9b7de94f61c59cdd4095e65ce16c307bb7507df
Surya-552 <109583394+Surya-552@users.noreply.github.com>
2023-02-11 12:25:28 +0530

    pushing project

---
+++ src/mycomponent/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import surya from '../image/surya.jpg'
+export default function Main() {
+  return (
+    <div>
+      <h1></h1>
+      <img src={surya} alt="JS"/>
+    </div>
+  )
+}
